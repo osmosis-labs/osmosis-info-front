38d4c4ed4996a6a6d22904875deef1f77b7c72f6
Dorian <dorian@latouche.dev>
2022-04-20 07:47:59 +0200

    update button connect wallet

--- src/components/appBar/button_connection.jsx
+++ src/components/appBar/button_connection.jsx
@@ -1,23 +1,32 @@
 import { Button, makeStyles } from "@material-ui/core"
 import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet"
+import { useKeplr } from "../../contexts/KeplrProvider"
 const useStyles = makeStyles((theme) => {
 	return {
 		rootButtonConnection: {
-			marginLeft:"16px",
+			marginLeft: "16px",
 			backgroundColor: "#322DC2",
-            color: theme.palette.primary.contrastText,
-            "&:hover": {
-                backgroundColor: "#4642C8",
-            }
+			color: theme.palette.primary.contrastText,
+			"&:hover": {
+				backgroundColor: "#4642C8",
+			},
 		},
 	}
 })
 
 const ButtonConnection = () => {
 	const classes = useStyles()
+	const { keplrStatus } = useKeplr()
+
+	const onClick = () => {}
 
 	return (
-		<Button variant="contained" className={classes.rootButtonConnection} startIcon={<AccountBalanceWalletIcon />}>
+		<Button
+			variant="contained"
+			onClick={onClick}
+			className={classes.rootButtonConnection}
+			startIcon={<AccountBalanceWalletIcon />}
+		>
 			Connect wallet
 		</Button>
 	)
