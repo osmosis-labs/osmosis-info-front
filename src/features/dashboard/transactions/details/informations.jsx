c62192f165f68d6e52180dd1b496275b254ee1db
Dorian <dorian@latouche.dev>
2022-04-22 10:34:04 +0200

    Add trx details informations

--- src/features/dashboard/transactions/details/informations.jsx
+++ src/features/dashboard/transactions/details/informations.jsx
@@ -1,6 +1,13 @@
 import { makeStyles } from "@material-ui/core"
 import dayjs from "dayjs"
+import { useToast } from "../../../../contexts/Toast.provider"
+import { IconButton } from "@mui/material"
+import FileCopyIcon from "@mui/icons-material/FileCopy"
 
+import CheckIcon from "@mui/icons-material/Check"
+import CloseIcon from "@mui/icons-material/Close"
+import { usePrices } from "../../../../contexts/PricesProvider"
+import { detectBestDecimalsDisplay, formateNumberDecimalsAuto } from "../../../../helpers/helpers"
 const useStyles = makeStyles((theme) => {
 	return {
 		rootInformations: {
@@ -9,8 +16,8 @@ const useStyles = makeStyles((theme) => {
 		title: {
 			color: theme.palette.primary.contrastText,
 			fontSize: "16px",
-			backgroundColor: theme.palette.primary.light,
-            padding: "12px 20px",
+			backgroundColor: theme.palette.primary.main2,
+			padding: "12px 20px",
 			borderRadius: "16px 16px 0 0",
 			borderBottom: `1px solid ${theme.palette.primary.light}`,
 		},
@@ -18,38 +25,150 @@ const useStyles = makeStyles((theme) => {
 			display: "flex",
 			flexDirection: "column",
 			justifyContent: "center",
-            padding: "8px 20px",
+			padding: "8px 20px",
 			alignItems: "flex-start",
-			backgroundColor: theme.palette.primary.main2,
+			backgroundColor: theme.palette.primary.light,
 		},
-		rowBorder: {
+		rowMiddle: {
+			borderBottom: `1px solid ${theme.palette.gray.dark}`,
+		},
+		rowEnd: {
+			padding: "0 20px 12px 20px",
+			borderRadius: "0 0 16px 16px ",
 		},
 		subTitle: {
 			fontSize: "14px",
-        },
+		},
 		info: {
-            width: "100%",
+			width: "100%",
 			fontSize: "12px",
-			borderBottom: `1px solid ${theme.palette.primary.light}`,
-            padding: "0 0 8px 0",
-            marginTop: "4px"
-        },
+			fontWeight: "bold",
+			padding: "0 0 8px 0",
+			marginTop: "4px",
+			color: theme.palette.gray.dark,
+		},
+		hashContainer: {
+			marginTop: "0",
+			display: "flex",
+			flexDirection: "row",
+			justifyContent: "space-between",
+			alignItems: "center",
+			width: "100%",
+		},
+		hash: {
+			cursor: "pointer",
+			overflow: "hidden",
+			textOverflow: "ellipsis",
+			color: `${theme.palette.table.link} !important`,
+		},
+		iconContainer: {
+			backgroundColor: `${theme.palette.primary.dark} !important`,
+			borderRadius: "50% !important",
+			padding: "0px",
+		},
+		iconHash: {
+			color: theme.palette.primary.light2,
+			backgroundColor: theme.palette.primary.dark,
+			fontSize: "14px !important",
+		},
+		textStatus: {
+			marginTop: "4px",
+			display: "flex",
+			flexDirection: "row",
+			alignItems: "center",
+			padding: "0 0 8px 0",
+			color: theme.palette.gray.dark,
+			fontWeight: "bold",
+			width: "100%",
+			fontSize: "12px",
+		},
+		iconStatus: {
+			fontSize: "16px !important",
+			marginRight: "8px",
+		},
+		success: {
+			color: theme.palette.green.text,
+		},
+		failed: {
+			color: theme.palette.error.main,
+		},
+		feesUnit: {
+			color: theme.palette.yellow.gold,
+			opacity: 0.7,
+		},
 	}
 })
 const Informations = ({ data }) => {
 	const classes = useStyles()
+	const { priceOsmoBrut } = usePrices()
+	const { showToast } = useToast()
 
-	let dateToShow = dayjs(data.time.value.toString()).format("h:m a, D MMM YYYY")
+	const onClickHash = () => {
+		window.open(`https://www.mintscan.io/osmosis/txs/${data.hash.value}`, "_blank")
+	}
+
+	const copyHash = () => {
+		try {
+			navigator.clipboard.writeText(data.hash.value)
+			showToast({
+				severity: "success",
+				text: "TxHash copied to clipboard",
+			})
+		} catch (e) {
+			console.log("%cinformations.jsx -> 66 ERROR: e", "background: #FF0000; color:#FFFFFF", e)
+			showToast({
+				severity: "warning",
+				text: "Cannot copy TxHash to clipboard",
+			})
+		}
+	}
+
+	let dateToShow = dayjs(data.time.value.toString()).format("YYYY-MM-DD HH:mm:ss")
+	let hashDisplay = data.hash.value.substring(0, 10) + "..." + data.hash.value.substring(data.hash.value.length - 10)
+	data.fees = 0.0032
+	let usdFees = formateNumberDecimalsAuto({ price: data.fees * priceOsmoBrut })
 	return (
 		<div className={classes.rootInformations}>
 			<p className={classes.title}>Informations</p>
 			<div className={`${classes.row}`}>
 				<p className={classes.subTitle}>Chain ID</p>
-				<p className={classes.info}>lapin</p>
+				<p className={`${classes.info}  ${classes.rowMiddle}`}>{data.chainId}</p>
 			</div>
-			<div className={`${classes.row} ${classes.rowBorder}`}>
-				<p className={classes.subTitle}>Chain ID</p>
-				<p className={classes.info}>lapin</p>
+			<div className={`${classes.row}`}>
+				<p className={classes.subTitle}>TxHash</p>
+				<div className={`${classes.info}  ${classes.rowMiddle} ${classes.hashContainer}`}>
+					<p className={`${classes.hash}`} onClick={onClickHash}>
+						{hashDisplay}
+					</p>
+					<IconButton aria-label="copy" className={classes.iconContainer} onClick={copyHash}>
+						<FileCopyIcon className={classes.iconHash} />
+					</IconButton>
+				</div>
+			</div>
+			<div className={`${classes.row} `}>
+				<p className={classes.subTitle}>Status</p>
+				<p className={` ${classes.textStatus} ${classes.rowMiddle}`} onClick={onClickHash}>
+					{data.status === "success" ? (
+						<CheckIcon className={` ${classes.iconStatus} ${classes.success}`} />
+					) : (
+						<CloseIcon className={` ${classes.iconStatus} ${classes.failed}`} />
+					)}
+					{data.status === "success" ? "Success" : "Failed"}
+				</p>
+			</div>
+			<div className={`${classes.row}`}>
+				<p className={classes.subTitle}>Network fee</p>
+				<p className={`${classes.info} ${classes.rowMiddle}`}>
+					{data.fees} <span className={classes.feesUnit}>OSMO</span> (${usdFees})
+				</p>
+			</div>
+			<div className={`${classes.row}`}>
+				<p className={classes.subTitle}>Height</p>
+				<p className={`${classes.info} ${classes.rowMiddle}`}>{data.height}</p>
+			</div>
+			<div className={`${classes.row} ${classes.rowEnd}`}>
+				<p className={classes.subTitle}>Time</p>
+				<p className={`${classes.info}`}>{`${data.time.display} (${dateToShow})`}</p>
 			</div>
 		</div>
 	)
