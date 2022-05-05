2bac67473ad1012425fe3e035645fa6cd9b9e349
Dorian <dorian@latouche.dev>
2022-05-05 13:37:10 +0200

    Fix safari keplr

--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
 import "./wdyr"
 
-import { isMobile } from "react-device-detect"
-import React, { Suspense } from "react"
+import React from "react"
 import ReactDOM from "react-dom"
 import * as serviceWorker from "./serviceWorker"
 import "./styles/index.css"
 import "./styles/loader.css"
 import "./styles/transitions.css"
-const AppDesktop = React.lazy(() => (!isMobile ? import("./features/app/app_desktop") : null))
-const AppMobile = React.lazy(() => (isMobile ? import("./features/app/app_mobile") : null))
+import AppDesktop from "./features/app/app_desktop"
 import { ThemeCustomProvider } from "./contexts/ThemeProvider"
 import { SettingsProviders } from "./contexts/SettingsProvider"
-import LoaderWait from "./components/appBar/loader_wait"
 
 ReactDOM.render(
 	<SettingsProviders>
 		<ThemeCustomProvider>
-			<Suspense fallback={<LoaderWait />}>{isMobile ? <AppMobile /> : <AppDesktop />}</Suspense>
+			<AppDesktop />
 		</ThemeCustomProvider>
 	</SettingsProviders>,
 	document.getElementById("root")
