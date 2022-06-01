b103c07284404a7ce6bf7678256dac3e4810f060
Dorian <dorian@latouche.dev>
2022-06-01 14:58:06 +0200

    fix  token overview

--- src/features/tokens/tokenOverview/tokenOverview.jsx
+++ src/features/tokens/tokenOverview/tokenOverview.jsx
@@ -1,6 +1,7 @@
 import { makeStyles } from "@material-ui/core"
 import { forwardRef, useEffect, useRef, useState } from "react"
 import Paper from "../../../components/paper/Paper"
+import { useGainers, useLosers } from "../../../hooks/data/metrics.hook"
 import Bar from "./bar"
 
 const useStyles = makeStyles((theme) => {
@@ -30,16 +31,29 @@ const useStyles = makeStyles((theme) => {
 const TokenOverview = () => {
 	const classes = useStyles()
 	const [stop, setStop] = useState(false)
+	const { data: losers, isLoading: isLoadingLosers } = useLosers()
+	const { data: gainers, isLoading: isLoadingGainers } = useGainers()
 	const refBar = useRef(null)
 	const refBar2 = useRef(null)
 
 	const onEnter = (e) => {
-		setStop(true)
+		setStop((s) => true)
 	}
 
 	const onLeave = (e) => {
-		setStop(false)
+		setStop((s) => false)
 	}
+
+	useEffect(() => {
+		if (refBar.current && refBar2.current &&!isLoadingLosers && !isLoadingGainers) {
+			window.setTimeout(() => {
+				setStop((s) => true)
+				setStop((s) => false)
+				
+			}, 333)
+		}
+	}, [refBar, refBar2, isLoadingLosers, isLoadingGainers])
+
 
 	useEffect(() => {
 		if (stop) {
@@ -51,7 +65,7 @@ const TokenOverview = () => {
 		}
 	}, [stop, refBar, refBar2])
 
-
+	console.log("tokenOverview.jsx (l:58): here:", stop)
 	return (
 		<div className={classes.rootTokenOverview} onMouseLeave={onLeave} onMouseEnter={onEnter}>
 			<Paper className={classes.rootTokenOverview}>
