c530f70a6421c2522a9a6128eb4f5b6c1294fae1
Dorian <dorian@latouche.dev>
2022-06-20 08:47:24 +0200

    Fix settings when is closed update state

--- src/contexts/debug.provider.js
+++ src/contexts/debug.provider.js
@@ -1,8 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react"
 const DebugContext = createContext()
 
-const MODE = process.env.REACT_APP_MODE
-
 export const useDebug = () => useContext(DebugContext)
 
 export const DebugProvider = ({ children }) => {
@@ -10,8 +8,19 @@ export const DebugProvider = ({ children }) => {
 	const onClose = () => setOpen(false)
 	const [isAccumulated, setIsAccumulated] = useState(true)
 	const [isStakingAccumulated, setIsStakingAccumulated] = useState(true)
-	const [message, setMessage] = useState("The Terra blockchain has resumed with on-chain swaps disabled and IBC channels closed. LUNA and UST pools rewards will drain shortly")
+	const [message, setMessage] = useState(
+		"The Terra blockchain has resumed with on-chain swaps disabled and IBC channels closed. LUNA and UST pools rewards will drain shortly"
+	)
 	const [messageLevel, setMessageLevel] = useState("info")
+
+	let MODE = "production"
+	if (
+		window.location.hostname === "localhost" ||
+		window.location.hostname === "osmosis.latouche.dev" ||
+		window.location.hostname === "osmosis2.latouche.dev"
+	) {
+		MODE = "dev"
+	}
 
 	const value = {
 		open,
--- src/features/dashboard/analytics/liquidity_reward/liquidity_reward.jsx
+++ src/features/dashboard/analytics/liquidity_reward/liquidity_reward.jsx
@@ -294,7 +294,14 @@ const LiquidityReward = () => {
 						</div>
 					</>
 				) : (
-					<p className={classes.textNotFound}>No data found.</p>
+					<>
+						<div className={classes.chartContainer}>
+							<p className={classes.textNotFound}>No data found.</p>
+						</div>
+						<div className={classes.chartInfo}>
+							<SelectToken tokens={tokens} onChangeToken={onChangeToken} currentToken={currentToken} />
+						</div>
+					</>
 				)}
 			</Paper>
 		</div>
