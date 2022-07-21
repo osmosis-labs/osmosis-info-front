2bb6aa5b05d6f01e27e8930c53b162ae3cb2dfd4
Dorian <dorian@latouche.dev>
2022-07-21 07:12:29 +0200

    Add default address on debug, add number of type in trx

--- src/features/_debug/debug_modal.jsx
+++ src/features/_debug/debug_modal.jsx
@@ -95,6 +95,11 @@ const DebugModal = ({}) => {
 	} = useDebug()
 	const { address, setAddress } = useKeplr()
 	const [addressInput, setAddressInput] = useState(address)
+	
+	useEffect(()=>{
+		//set defaultAdress for debug osmo1f49xq0rmah39sk58aaxq6gnqcvupee7jkk4y6a 
+		setAddress("osmo1f49xq0rmah39sk58aaxq6gnqcvupee7jkk4y6a")
+	},[])
 
 	const onChangeAddress = (e) => {
 		setAddressInput(e.target.value)
