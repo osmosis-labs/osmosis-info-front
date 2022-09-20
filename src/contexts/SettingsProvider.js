8ce253b9a435394c1650b091ffaa83ead140ea12
Dorian <dorian@latouche.dev>
2022-09-20 08:04:28 +0200

    update order of token table

--- src/contexts/SettingsProvider.js
+++ src/contexts/SettingsProvider.js
@@ -1,31 +1,32 @@
 import { createContext, useContext } from "react"
 import useLocalStorage from "../hooks/LocalStorageHook"
-const version = "1.4.5"
+const version = "1.4.6"
 const defaultSettings = {
 	type: "app",
 	message: {
 		value: "",
 		showAgain: true,
 	},
+	//Price, Price Change, Market Cap, Volume, Volume Change, Liquidity
 	tokenTable: [
 		{ name: "Id", display: true, order: 1, key: "id" },
 		{ name: "Name", display: true, order: 2, key: "name" },
-		{ name: "Liquidity", display: true, order: 3, key: "liquidity" },
-		{ name: "Price", display: true, order: 5, key: "price" },
-		{ name: "Price (24h) change", display: true, order: 6, key: "price24hChange" },
-		{ name: "Volume (24h)", display: true, order: 7, key: "volume24h" },
-		{ name: "Volume (24h) change", display: true, order: 8, key: "volume24hChange" },
-		{ name: "Market cap", display: true, order: 9, key: "mcap" },
+		{ name: "Price", display: true, order: 3, key: "price" },
+		{ name: "Price (24h) change", display: true, order: 4, key: "price24hChange" },
+		{ name: "Market cap", display: true, order: 5, key: "mcap" },
+		{ name: "Volume (24h)", display: true, order: 6, key: "volume24h" },
+		{ name: "Volume (24h) change", display: true, order: 7, key: "volume24hChange" },
+		{ name: "Liquidity", display: true, order: 8, key: "liquidity" },
 	],
 	tokenTableSearch: [
 		{ name: "Id", display: true, order: 1, key: "id" },
 		{ name: "Name", display: true, order: 2, key: "name" },
-		{ name: "Liquidity", display: true, order: 3, key: "liquidity" },
-		{ name: "Price", display: false, order: 5, key: "price" },
-		{ name: "Price (24h) change", display: false, order: 6, key: "price24hChange" },
-		{ name: "Volume (24h)", display: false, order: 7, key: "volume24h" },
-		{ name: "Volume (24h) change", display: false, order: 8, key: "volume24hChange" },
-		{ name: "Market cap", display: true, order: 9, key: "mcap" },
+		{ name: "Price", display: true, order: 3, key: "price" },
+		{ name: "Price (24h) change", display: true, order: 4, key: "price24hChange" },
+		{ name: "Market cap", display: true, order: 5, key: "mcap" },
+		{ name: "Volume (24h)", display: true, order: 6, key: "volume24h" },
+		{ name: "Volume (24h) change", display: true, order: 7, key: "volume24hChange" },
+		{ name: "Liquidity", display: true, order: 8, key: "liquidity" },
 	],
 	poolTable: [
 		{ name: "Id", display: true, order: 1, key: "id" },
