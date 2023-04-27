8a06ebcefbc7e7f1e920db5e89c279694719e177
Dorian <dorian@latouche.dev>
2023-04-27 07:42:02 +0200

    Add variant, disabled option, stroke option for icon button

--- packages/osmosis-info-ui/src/components/buttons/icon-button/icon-button.tsx
+++ packages/osmosis-info-ui/src/components/buttons/icon-button/icon-button.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 /**
  * Button with only one icon
@@ -11,19 +11,59 @@ export interface IconButtonProps {
 	Icon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
 	/** Calback when the button was clicked. */
 	onClick?: React.MouseEventHandler<HTMLDivElement>;
+	/** use stroke animation*/
+	strokeAnimation?: boolean;
+	/** use fill animation*/
+	fillAnimation?: boolean;
+	/** If the button can be used.*/
+	disabled?: boolean;
+	/** If flat then remove background, just display icon.*/
+	variant?: "default" | "flat";
 	/** Classname passed to the button.*/
 	className?: string;
 }
 
-export const IconButton = ({ Icon, onClick, className }: IconButtonProps) => {
-	const classIconButton = `
-    px-4 py-3 bg-main-700 rounded-xl h-fit w-fit cursor-pointer transition-colors hover:bg-main-600 [&>svg]:hover:fill-white-full 
-    [&>svg]:fill-main-400 
+export const IconButton = ({
+	Icon,
+	onClick,
+	className,
+	strokeAnimation,
+	fillAnimation = true,
+	disabled,
+	variant = "default",
+}: IconButtonProps) => {
+	const onClickButton = useCallback(
+		(e: any) => {
+			if (!disabled && onClick) onClick(e);
+		},
+		[onClick, disabled]
+	);
+
+	let classIconButton = `
+    px-4 py-3 rounded-xl h-fit w-fit cursor-pointer transition-colors 
     ${className}`;
 
+	const fillAnime = `[&>svg]:hover:fill-white-full [&>svg]:fill-main-400 `;
+	const fill = `[&>svg]:fill-main-400 [&>svg]:transition-colors`;
+	const strokeAnime = `[&>svg]:hover:stroke-white-full [&>svg]:stroke-main-400 `;
+	const stroke = `[&>svg]:stroke-main-400 [&>svg]:transition-colors`;
+	if (variant === "default") {
+		classIconButton = `bg-main-700 hover:bg-main-600 ${classIconButton}`;
+	}
+	if (fillAnimation) {
+		classIconButton = `${fill} ${classIconButton}`;
+		if (!disabled) classIconButton = ` ${fillAnime} ${classIconButton}`;
+		else classIconButton = `opacity-50 ${classIconButton}`;
+	}
+	if (strokeAnimation) {
+		classIconButton = `${stroke} ${classIconButton}`;
+
+		if (!disabled) classIconButton = ` ${strokeAnime} ${classIconButton} `;
+		else classIconButton = `opacity-50 ${classIconButton}`;
+	}
 	return (
-		<div onClick={onClick} className={classIconButton}>
-			<Icon height={20} width={21} className="duration-default" />
+		<div onClick={onClickButton} className={classIconButton}>
+			<Icon height={20} width={21} className="" />
 		</div>
 	);
 };
